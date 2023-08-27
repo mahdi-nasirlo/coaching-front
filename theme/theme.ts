352dc7684096e87780b09c4a784cba66727c2947
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-27 11:11:27 +0330

    initial commit

---
+++ theme/theme.ts
@@ -0,0 +1,11 @@
+import type {ThemeConfig} from 'antd';
+
+const customTheme: ThemeConfig = {
+    token: {
+        fontSize: 16,
+        fontFamily: "IRANSans",
+        colorPrimary: 'red',
+    },
+};
+
+export default customTheme;
