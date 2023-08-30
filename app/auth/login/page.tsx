767f3e618ec7db9afa3aa5bf56f4291a33c30ed8
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2023-08-30 10:52:21 +0330

    created login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,62 +1,94 @@
+"use client"
+
 import Image from "next/image";
+import {Button, Col, Form, Input, Row} from "antd";
+import {loginInterface, loginRequest} from "@/services/auth/login";
+import useForm from "antd/lib/form/hooks/useForm";
+import {useState} from "react";
+import ThemeProvider from "@/providers/theme-provider";
+
+
+type Status = { status: "success" | "error" | "warning" | "validating" | undefined, msg: string }
 
 export default function Login() {
+
+    const [accountValidate, setAccountValidate] = useState<Status>({status: undefined, msg: ""})
+
+    const [form] = useForm()
+    const handleSubmit = (values: loginInterface) => {
+
+        const res = loginRequest(values)
+            .catch((error) => {
+                setAccountValidate({status: "error", msg: error.response.data.message})
+            })
+
+    }
+
     return <>
-        <section className="bg-gray-50 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <Image src="/statics/mark.svg" alt="logo" width="80" height="80" className="w-15 h-15 mr-2"/>
-                </a>
-                <div
-                    className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Sign in to your account
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
-                            <div>
-                                <label htmlFor="email"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
-                                    email</label>
-                                <input type="email" name="email" id="email"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                       placeholder="name@company.com"/>
-                            </div>
-                            <div>
-                                <label htmlFor="password"
-                                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••"
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-                            </div>
-                            <div className="flex items-center justify-between">
-                                <div className="flex items-start">
-                                    <div className="flex items-center h-5">
-                                        <input id="remember" aria-describedby="remember" type="checkbox"
-                                               className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                                        />
-                                    </div>
-                                    <div className="ml-3 text-sm">
-                                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember
-                                            me</label>
-                                    </div>
-                                </div>
-                                <a href="#"
-                                   className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
-                                    password?</a>
-                            </div>
-                            <button type="submit"
-                                    className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
-                                in
-                            </button>
-                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                Don’t have an account yet? <a href="#"
-                                                              className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
-                                up</a>
-                            </p>
-                        </form>
+        <ThemeProvider>
+            <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                        <Image src="/statics/mark.svg" alt="logo" width="80" height="80" className="w-15 h-15 mr-2"/>
+                    </a>
+                    <div
+                        className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                ورود به حساب کاربری
+                            </h1>
+                            <Form
+                                form={form}
+                                onFinish={handleSubmit}
+                                className="space-y-4 md:space-y-6"
+                                requiredMark="optional"
+                                name="form_item_path"
+                                layout="vertical"
+                            >
+                                <Row gutter={[32, 12]}>
+                                    <Col span={24}>
+                                        <Form.Item
+                                            name="email"
+                                            label="ایمیل"
+                                            rules={[
+                                                {required: true, message: "لطفا فیلد را وارد نمایید"},
+                                                {type: "string"},
+                                            ]}
+                                            validateStatus={accountValidate.status}
+                                            help={accountValidate.msg}
+                                        >
+                                            <Input
+                                                size="large"
+                                                placeholder="email@example.com"
+                                            />
+                                        </Form.Item>
+                                    </Col>
+                                    <Col span={24}>
+                                        <Form.Item
+                                            name="password"
+                                            label="گذرواژه"
+                                            rules={[
+                                                {
+                                                    required: true,
+                                                    message: "لطفا فیلد را وارد نمایید",
+                                                },
+                                                {type: "string"},
+                                            ]}
+                                        >
+                                            <Input size="large" placeholder={"وارد نمایید"}/>
+                                        </Form.Item>
+                                    </Col>
+                                    <Col className="mt-2" span={24}>
+                                        <Button size="large" className="w-full" htmlType="submit" type="primary">
+                                            ورود
+                                        </Button>
+                                    </Col>
+                                </Row>
+                            </Form>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </ThemeProvider>
     </>
 }
