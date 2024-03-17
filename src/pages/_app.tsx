212bfc78ce2ec8f44670f5bc48eabade6238b6c8
mahdi-nasirlo <72885979+mahdi-nasirlo@users.noreply.github.com>
2024-03-18 01:35:24 +0330

    Add:: proccess bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import {ElementType, useEffect} from "react";
-import {useRouter} from "next/router";
-import type {AppProps} from "next/app";
+import { ElementType, useEffect } from "react";
+import { useRouter } from "next/router";
+import type { AppProps } from "next/app";
 import SEO from "@components/seo/deafult-seo";
 import FallbackLayout from "@layout/fallback";
-import {Toaster} from "react-hot-toast";
-
+import { Toaster } from "react-hot-toast";
+import tailwindConfig from "../../tailwind.config";
 
 import "@assets/css/font-awesome.min.css";
 import "@assets/css/font-linea.css";
@@ -12,10 +12,11 @@ import "@assets/css/fonts.css";
 import "@assets/css/tailwind.css";
 import "@assets/css/swiper.css";
 
-import {UIProvider} from "../contexts/ui-context";
-import {UserProvider} from "../contexts/user-context";
+import { UIProvider } from "../contexts/ui-context";
+import { UserProvider } from "../contexts/user-context";
 import QueryClientProvider from "../providers/query-client-provider";
 import AuthProvider from "../providers/auth-provider";
+import { PagesProgressBar as ProgressBar } from "next-nprogress-bar";
 
 interface CustomAppProps extends Omit<AppProps, "Component"> {
     Component: AppProps["Component"] & { Layout: ElementType };
@@ -24,16 +25,15 @@ interface CustomAppProps extends Omit<AppProps, "Component"> {
     };
 }
 
-const MyApp = ({Component, pageProps}: CustomAppProps) => {
+const MyApp = ({ Component, pageProps }: CustomAppProps) => {
     const router = useRouter();
     const Layout = Component.Layout || FallbackLayout;
     const layoutProps =
         typeof pageProps.layout === "object" ? pageProps.layout : {};
 
     useEffect(() => {
-
         document.activeElement instanceof HTMLElement &&
-        document.activeElement.blur();
+            document.activeElement.blur();
     }, [router]);
 
     useEffect(() => {
@@ -45,9 +45,16 @@ const MyApp = ({Component, pageProps}: CustomAppProps) => {
             <QueryClientProvider>
                 <UIProvider>
                     <UserProvider>
-                        <Toaster containerStyle={{zIndex: 999999}}/>
+                        <Toaster containerStyle={{ zIndex: 999999 }} />
+                        <ProgressBar
+                            height="3px"
+                            color={
+                                tailwindConfig.theme.extend.colors.primary
+                                    .DEFAULT
+                            }
+                        />
                         <Layout {...layoutProps}>
-                            <SEO/>
+                            <SEO />
                             <Component {...pageProps} />
                         </Layout>
                     </UserProvider>
